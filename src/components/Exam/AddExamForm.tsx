5041c5690825bb70e449491b0c60308704aab97e
datmt <datmt2003@vnpt.vn>
2026-01-22 15:39:21 +0700

    update

--- src/components/Exam/AddExamForm.tsx
+++ src/components/Exam/AddExamForm.tsx
@@ -144,15 +144,10 @@ export default function AddExamForm() {
               <span className="block text-xs font-bold text-gray-800">Giám thị AI (Camera)</span>
               <span className="block text-[10px] text-gray-500">Chống gian lận</span>
             </div>
-            <label className="relative inline-flex items-center cursor-pointer">
-              <input
-                type="checkbox"
-                checked={isAIProctoring}
-                onChange={(e) => setIsAIProctoring(e.target.checked)}
-                className="sr-only peer"
-              />
+            <div className="relative inline-flex items-center cursor-pointer">
+              <input type="checkbox" checked={isAIProctoring} onChange={(e) => setIsAIProctoring(e.target.checked)} className="sr-only peer" />
               <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-red-500"></div>
-            </label>
+            </div>
           </div>
         </div>
 
